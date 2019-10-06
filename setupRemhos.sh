b04800e928189fda7eede61d42205065877c77a3
Arturo <artv3.89@gmail.com>
2019-10-05 22:02:37 -0700

    setup script to mfem-4

---
+++ setupRemhos.sh
@@ -0,0 +1,21 @@
+git checkout use-mfem-master && cd ../ &&
+wget https://computing.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.10.0b.tar.gz &&
+tar -zxvf hypre-2.10.0b.tar.gz &&    
+cd hypre-2.10.0b/src/ &&
+./configure --disable-fortran &&
+make -j 3 &&
+cd ../.. && mv hypre-2.10.0b hypre &&
+wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz &&
+tar -zxvf metis-4.0.3.tar.gz && cd metis-4.0.3 &&
+make -j 3 && cd .. &&
+ln -s metis-4.0.3 metis-4.0 &&
+git clone --recursive https://github.com/mfem/mfem.git &&
+cd mfem && make parallel -j 3
+cd ../Remhos && make -j 3
+
+
+
+
+
+    
+    
